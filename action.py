c7247e0696e8169e4127515e56327bd5c9ee446a
Michal Przytulski <michal@przytulski.pl>
2022-02-16 22:31:37 +0100

    return repository if exists

--- action.py
+++ action.py
@@ -10,15 +10,29 @@ def main():
     image_tag_mutability = os.environ["INPUT_IMAGE_TAG_MUTABILITY"]
 
     ecr = boto3.client("ecr")
-    response = ecr.create_repository(
-        repositoryName=repository_name,
-        imageTagMutability=image_tag_mutability,
-        imageScanningConfiguration={
-            'scanOnPush': bool(scan_on_push)
-        }
+
+    response = ecr.describe_repositories(
+        repositoryNames=[
+            'repository_name',
+        ],
     )
 
+    repository = next(iter(response.get("repositories")), None)
+
+    if not repository:
+        response = ecr.create_repository(
+            repositoryName=repository_name,
+            imageTagMutability=image_tag_mutability,
+            imageScanningConfiguration={
+                'scanOnPush': bool(scan_on_push)
+            }
+        )
+        repository = response.get("repository")
+
+    repository_url = repository.get("repositoryUri")
+
     print(response)
+    print(f"::set-output name=repositry::{repository_url}")
 
 
 if __name__ == "__main__":
